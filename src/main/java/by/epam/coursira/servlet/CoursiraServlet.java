7cf0952eae9ef7a200ad7f9fc0918712d8fd5ff9
Elena Dorogush <Elena_Dorogush@epam.com>
2019-09-20 16:23:21 +0300

    add c3po

--- src/main/java/by/epam/coursira/servlet/CoursiraServlet.java
+++ src/main/java/by/epam/coursira/servlet/CoursiraServlet.java
@@ -3,44 +3,21 @@ package by.epam.coursira.servlet;
 import by.epam.coursira.command.Command;
 import by.epam.coursira.command.CommandFactory;
 import by.epam.coursira.command.CommandResult;
-import by.epam.coursira.command.CourseCommand;
-import by.epam.coursira.command.CourseCreateCommand;
-import by.epam.coursira.command.CourseIdCommand;
-import by.epam.coursira.command.CourseIdSubscriptionCommand;
-import by.epam.coursira.command.CourseUpdateCommand;
-import by.epam.coursira.command.IndexCommand;
-import by.epam.coursira.command.LanguageCommand;
-import by.epam.coursira.command.LoginCommand;
-import by.epam.coursira.command.LogoutCommand;
-import by.epam.coursira.command.PersonalPageCommand;
-import by.epam.coursira.command.PersonalUpdateCommand;
-import by.epam.coursira.command.RegistrationCommand;
-import by.epam.coursira.command.RegistrationConfirmCommand;
-import by.epam.coursira.dao.CourseDao;
-import by.epam.coursira.dao.StudentDao;
-import by.epam.coursira.dao.UserDao;
 import by.epam.coursira.entity.Principal;
 import by.epam.coursira.exception.ClientCommandException;
 import by.epam.coursira.exception.PageNotFoundException;
 import by.epam.coursira.exception.PoolConnectionException;
 import by.epam.coursira.exception.ServiceException;
-import by.epam.coursira.mail.MailSender;
 import by.epam.coursira.model.ErrorModel;
 import by.epam.coursira.pool.ConnectionPoolImpl;
-import by.epam.coursira.security.BCryptHashMethod;
-import by.epam.coursira.service.CourseModificationService;
-import by.epam.coursira.service.CourseService;
 import by.epam.coursira.service.PrincipalService;
-import by.epam.coursira.service.UserService;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.time.Duration;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -53,6 +30,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -97,67 +75,21 @@ public class CoursiraServlet extends HttpServlet {
     ServletContext context = springContext.getBean(ServletContext.class);
 
     // initParams
-    final String url = context.getInitParameter("jdbcDriver");
-    logger.info(url);
-    final int dbPoolSize = Integer.parseInt(context.getInitParameter("dbPoolSize"));
     final int cleanerInitialDelay =
       Integer.parseInt(context.getInitParameter("cleanerInitialDelay"));
-
-    final Duration sessionLoginDuration =
-      Duration.ofHours(Integer.parseInt(context.getInitParameter("sessionLoginDurationHours")));
-    final Duration sessionAnonymousDuration =
-      Duration.ofHours(
-        Integer.parseInt(context.getInitParameter("sessionAnonymousDurationHours")));
     final int cleanerProcedureDelay = Integer.parseInt(context.getInitParameter("cleanerDelay"));
-    final String gmailPassword = context.getInitParameter("gmailPassword");
-    final String gmailAddress = context.getInitParameter("gmailAddress");
-    final Properties propSmtp = new Properties();
-    propSmtp.put("mail.smtp.host", context.getInitParameter("mail.smtp.host"));
-    propSmtp.put("mail.smtp.port", context.getInitParameter("mail.smtp.port"));
-    propSmtp.put("mail.smtp.auth", context.getInitParameter("mail.smtp.auth"));
-    propSmtp.put(
-      "mail.smtp.starttls.enable", context.getInitParameter("mail.smtp.starttls.enable"));
-    final int paginationLimit = Integer.parseInt(context.getInitParameter("paginationLimit"));
-
-    try {
-      Class.forName("org.postgresql.Driver");
-      connectionPool = new ConnectionPoolImpl(dbPoolSize, url);
-      logger.info("pool constructed");
-    } catch (PoolConnectionException | ClassNotFoundException e) {
-      throw new ServletException(e);
-    }
-    CourseDao courseDao = new CourseDao(connectionPool);
-    StudentDao studentDao = new StudentDao(connectionPool);
-    UserDao userDao = new UserDao(connectionPool);
-//    CourseDao courseDao = springContext.getBean(CourseDao.class);
-//    StudentDao studentDao = springContext.getBean(StudentDao.class);
-//    UserDao userDao = springContext.getBean(UserDao.class);
-    MailSender mailSender = new MailSender(gmailAddress, gmailPassword, propSmtp);
-    principalService =
-      new PrincipalService(
-        userDao,
-        sessionLoginDuration,
-        sessionAnonymousDuration,
-        new BCryptHashMethod(),
-        mailSender);
-    CourseService courseService = new CourseService(courseDao, studentDao, userDao);
-    UserService userService = new UserService(userDao);
-    CourseModificationService courseModificationService =
-      new CourseModificationService(courseDao, userDao, mailSender);
-
-    commandList.add(new CourseCommand(courseService, userService, paginationLimit));
-    commandList.add(new IndexCommand(courseService));
-    commandList.add(new LanguageCommand(principalService));
-    commandList.add(new LoginCommand(principalService));
-    commandList.add(new LogoutCommand(principalService));
-    commandList.add(new CourseIdCommand(courseService, paginationLimit));
-    commandList.add(new CourseIdSubscriptionCommand(courseService));
-    commandList.add(new PersonalPageCommand(courseService, paginationLimit));
-    commandList.add(new PersonalUpdateCommand(userService));
-    commandList.add(new RegistrationCommand(principalService));
-    commandList.add(new RegistrationConfirmCommand(principalService));
-    commandList.add(new CourseCreateCommand(courseModificationService));
-    commandList.add(new CourseUpdateCommand(courseModificationService, courseService, userService));
+    //connectionPool = springContext.getBean(ConnectionPoolImpl.class);
+//    try {
+//      Class.forName("org.postgresql.Driver");
+//      //connectionPool = new ConnectionPoolImpl(dbPoolSize, url);
+//      connectionPool = springContext.getBean(ConnectionPoolImpl.class);
+//      logger.info("pool constructed");
+//    } catch (PoolConnectionException | ClassNotFoundException e) {
+//      throw new ServletException(e);
+//    }
+//
+    principalService = springContext.getBean(PrincipalService.class);
+    commandList = springContext.getBean("commandList", List.class);
     logger.info("commandList constructed ");
 
     scheduledFuture =
